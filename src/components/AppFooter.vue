918a03eb978b1255b1eebfd2b412f403a37e422d
Mucsinator <vidazsombor66@gmail.com>
2024-03-26 08:54:21 +0100

    Social Media elemek és fiókok elkészírése

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,22 +1,21 @@
 <template>
   <footer class="footer">
     <ul class="social-icon">
-      <li class="social-icon__item"><a class="social-icon__link" href="#">
+      <li class="social-icon__item"><a class="social-icon__link" target="_blank" href="https://www.facebook.com/people/GameStrix-Web%C3%A1ruh%C3%A1z/61557614525724/
+">
           <ion-icon name="logo-facebook"></ion-icon>
         </a></li>
-      <li class="social-icon__item"><a class="social-icon__link" href="#">
+      <li class="social-icon__item"><a class="social-icon__link" target="_blank" href="https://discord.gg/d57TACK389">
           <ion-icon name="logo-discord"></ion-icon>
         </a></li>
-      <li class="social-icon__item"><a class="social-icon__link" href="#">
-          <ion-icon name="logo-linkedin"></ion-icon>
+      <li class="social-icon__item"><a class="social-icon__link"  href="mailto:info.gamestrix@gmail.com">
+        <ion-icon name="mail-outline"></ion-icon>
         </a></li>
-      <li class="social-icon__item"><a class="social-icon__link" href="#">
-          <ion-icon name="logo-instagram"></ion-icon>
-        </a></li>
+      
     </ul>
     <ul class="menu">
-      <li class="menu__item"><a class="menu__link" href="/">Home</a></li>
-      <li class="menu__item"><a class="menu__link" href="#">About</a></li>
+      <li  class="menu__item"><a class="menu__link" href="/">Home</a></li>
+      <li class="menu__item"><a class="menu__link" href="">About</a></li>
       <li class="menu__item"><a class="menu__link" href="#">Services</a></li>
       <li class="menu__item"><a class="menu__link" href="#">Team</a></li>
       <li class="menu__item"><a class="menu__link" href="#">Contact</a></li>
