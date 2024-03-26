5a9d8002bafb87ec38857d52d7438f2d037fdd8b
Mucsinator <vidazsombor66@gmail.com>
2024-03-26 11:06:54 +0100

    Duplás ajánlatok  , főoldal fix,média fix

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -13,14 +13,14 @@
         </a></li>
       
     </ul>
-    <ul class="menu">
+    <!-- <ul class="menu">
       <li  class="menu__item"><a class="menu__link" href="/">Home</a></li>
       <li class="menu__item"><a class="menu__link" href="">About</a></li>
       <li class="menu__item"><a class="menu__link" href="#">Services</a></li>
       <li class="menu__item"><a class="menu__link" href="#">Team</a></li>
       <li class="menu__item"><a class="menu__link" href="#">Contact</a></li>
 
-    </ul>
+    </ul> -->
     <p>&copy;2024 GameStrix webáruház</p>
     <p>Boglári Bence | Horváth Máté | Vida Csaba Zsombor</p>
   </footer>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -32,24 +32,20 @@
               aria-label="Slide 3"></button>
             <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3"
               aria-label="Slide 4"></button>
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="4"
-              aria-label="Slide 5"></button>
+            
           </div>
           <div class="carousel-inner"> <!-- Adjust carousel height here -->
             <div class="carousel-item active">
-              <img src="../assets/img/1.png" class="d-block w-100" alt="...">
+              <img src="../assets/img/1carou.png" class="d-block w-100" alt="...">
             </div>
             <div class="carousel-item">
-              <img src="../assets/img/1.png" class="d-block w-100" alt="...">
+              <img src="../assets/img/2carou.png" class="d-block w-100" alt="...">
             </div>
             <div class="carousel-item">
-              <img src="../assets/img/1.png" class="d-block w-100" alt="...">
+              <img src="../assets/img/3carou.png" class="d-block w-100" alt="...">
             </div>
             <div class="carousel-item">
-              <img src="../assets/img/1.png" class="d-block w-100" alt="...">
-            </div>
-            <div class="carousel-item">
-              <img src="../assets/img/1.png" class="d-block w-100" alt="...">
+              <img src="../assets/img/4carou.png" class="d-block w-100" alt="...">
             </div>
           </div>
           <!-- <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators"
@@ -79,6 +75,54 @@
         </div>
       </div>
     </div>
+
+
+    <br>
+    <br>
+    <br>
+
+
+    <h1 style="text-align: center;"> Legkedveltebb Párosok!</h1>
+
+
+    <br>
+    <br>
+    <br>
+
+    <div class="row">
+      <div class="col-md-4">
+        <div class="card mb-3">
+          <img src="../assets/img/kicsi.png" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Kezdő Gamereknek</h5>
+            <p class="card-text">GeForce RTX 4060 + Intel i5-12400F</p>
+            <a href="#" class="btn btn-primary">Kosárba</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="card mb-3">
+          <img src="../assets/img/közepes.png" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Haladó Gamereknek </h5>
+            <p class="card-text">GeForce RTX 4070 + Intel i5-12500</p>
+            <a href="#" class="btn btn-primary">Kosárba</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="card mb-3">
+          <img src="../assets/img/nagy.png" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Veterán Gamereknek</h5>
+            <p class="card-text">GeForce RTX 4090 + Intel Core i5-12600K</p>
+            <a href="#" class="btn btn-primary">Kosárba</a>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
 
   </div>
 </template>
